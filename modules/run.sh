5f7af947e7b7d3e65e4de905b1d98374ac56e41b
iceqiw <qqwei1123@163.com>
2018-08-04 11:31:11 +0800

    name

--- modules/run.sh
+++ modules/run.sh
@@ -1,1 +1,1 @@
-mvn archetype:generate -B   -DarchetypeGroupId=com.cwgj   -DarchetypeArtifactId=cwgj-service-archetype-modules   -DarchetypeVersion=0.0.1-SNAPSHOT   -DappName=test   -Dversion=1.0.0   -DarchetypeCatalog=local
+mvn archetype:generate -B   -DarchetypeGroupId=com.gofine  -DarchetypeArtifactId=gofine-archetype-modules   -DarchetypeVersion=0.0.1-SNAPSHOT   -DappName=test   -Dversion=1.0.0   -DarchetypeCatalog=local
